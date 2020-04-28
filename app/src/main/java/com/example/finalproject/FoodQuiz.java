b40e60ea92a89acbb5d10870891bd43791df64d3
Priyank Jain <priyank3@illinois.edu>
2020-04-28 00:06:05 -0500

    some buttons work

--- app/src/main/java/com/example/finalproject/FoodQuiz.java
+++ app/src/main/java/com/example/finalproject/FoodQuiz.java
@@ -1,12 +1,25 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FoodQuiz extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.quiz_food);
+
+        final Button backButton = findViewById(R.id.foodBack);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent intent = new Intent(FoodQuiz.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/finalproject/SportsQuiz.java
+++ app/src/main/java/com/example/finalproject/SportsQuiz.java
@@ -1,6 +1,9 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +11,15 @@ public class SportsQuiz extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.quiz_sport);
+
+        final Button backButton = findViewById(R.id.sportBack);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent intent = new Intent(SportsQuiz.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
