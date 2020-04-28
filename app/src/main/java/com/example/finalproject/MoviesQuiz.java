a40e346df0a830ef0bbde7cd34821bddc73da29c
Alec McKay <jamckay3@illinois.edu>
2020-04-28 00:09:46 -0500

    changed layout to corresponding quiz for each java class

--- app/src/main/java/com/example/finalproject/MoviesQuiz.java
+++ app/src/main/java/com/example/finalproject/MoviesQuiz.java
@@ -8,6 +8,6 @@ public class MoviesQuiz extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.quiz_movies);
     }
 }
--- app/src/main/java/com/example/finalproject/UiucQuiz.java
+++ app/src/main/java/com/example/finalproject/UiucQuiz.java
@@ -8,6 +8,6 @@ public class UiucQuiz extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.quiz_uiuc);
     }
 }
