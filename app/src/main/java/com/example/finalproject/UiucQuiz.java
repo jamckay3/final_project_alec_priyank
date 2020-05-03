9c678d7cc787e9c8ed359fa06a2d32047007f334
Alec McKay <jamckay3@illinois.edu>
2020-05-03 02:01:15 -0500

    fixed uiuc button and named 4th radiogroup in uiuc

--- app/src/main/java/com/example/finalproject/UiucQuiz.java
+++ app/src/main/java/com/example/finalproject/UiucQuiz.java
@@ -27,5 +27,44 @@ public class UiucQuiz extends AppCompatActivity {
                 finish();
             }
         });
+
+        q1RadioGroup = findViewById(R.id.uiucQ1RadioGroup);
+        q2RadioGroup = findViewById(R.id.uiucQ2RadioGroup);
+        q3RadioGroup = findViewById(R.id.uiucQ3RadioGroup);
+        q4RadioGroup = findViewById(R.id.uiucQ4RadioGroup);
+
+
+        final Button submitButton = findViewById(R.id.uiucSubmit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                int score = 0;
+                // code here executes when submit button is pressed
+                // want to check each answer's correct radiobutton
+                // if the correct answer is selected, add one to tmp score var
+                int q1Id = q1RadioGroup.getCheckedRadioButtonId();
+                int q2Id = q2RadioGroup.getCheckedRadioButtonId();
+                int q3Id = q3RadioGroup.getCheckedRadioButtonId();
+                int q4Id = q4RadioGroup.getCheckedRadioButtonId();
+
+                if (q1Id == R.id.uiucQ1Correct) {
+                    score++;
+                }
+                if (q2Id == R.id.uiucQ2Correct) {
+                    score++;
+                }
+                if (q3Id == R.id.uiucQ3Correct) {
+                    score++;
+                }
+                if (q4Id == R.id.uiucQ4Correct) {
+                    score++;
+                }
+                QuizScore.setQuizScore(score);
+                QuizScore.setCurrentQuiz("Science");
+                // now i set the quizScore var to the current score
+                Intent intent = new Intent(UiucQuiz.this, QuizScore.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
